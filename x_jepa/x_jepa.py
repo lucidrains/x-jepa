608222ac1147dce4a367fae3b2ff5755aba5e63b
lucidrains <lucidrains@gmail.com>
2026-07-09 10:39:32 -0700

    start with a necessary ingredient

--- x_jepa/x_jepa.py
+++ x_jepa/x_jepa.py
@@ -1,8 +1,16 @@
 from __future__ import annotations
+from functools import partial
 
 import torch
 from torch import nn
-from torch.nn import Module, ModuleList
+from torch.nn import Module, ModuleList, Linear
+
+from einops import einsum
+from einops.layers.torch import Rearrange
+
+# constants
+
+LinearNoBias = partial(Linear, bias = False)
 
 # helpers
 
@@ -12,6 +20,54 @@ def exists(v):
 def default(v, d):
     return v if exists(v) else d
 
+# attention
+
+class Attention(Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8,
+        causal = True
+    ):
+        super().__init__()
+        self.scale = dim_head ** -0.5
+        dim_inner = dim_head * heads
+        self.causal = causal
+
+        self.to_q = LinearNoBias(dim, dim_inner)
+        self.to_kv = LinearNoBias(dim, dim_inner * 2)
+
+        self.split_heads = Rearrange('b n (h d) -> b h n d', d = dim_head)
+        self.merge_heads = Rearrange('b h n d -> b n (h d)')
+
+        self.to_out = LinearNoBias(dim_inner, dim)
+
+    def forward(
+        self,
+        tokens # (b n d)
+    ):
+        device = tokens.device
+
+        q, k, v = (self.to_q(tokens), *self.to_kv(tokens).chunk(2, dim = -1))
+        q, k, v = (self.split_heads(t) for t in (q, k, v))
+
+        q = q * self.scale
+
+        sim = einsum(q, k, 'b h i d, b h j d -> b h i j')
+
+        if self.causal:
+            i, j = sim.shape[-2:]
+            causal_mask = torch.ones((i, j), dtype = torch.bool, device = device).triu(j - i + 1)
+            sim = sim.masked_fill(causal_mask, -torch.finfo(sim.dtype).max)
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum(attn, v, 'b h i j, b h j d -> b h i d')
+
+        out = self.merge_heads(out)
+        return self.to_out(out)
+
 # classes
 
 class WorldModel(Module):
@@ -20,5 +76,8 @@ class WorldModel(Module):
     ):
         super().__init__()
 
-    def forward(self, state):
+    def forward(
+        self,
+        state
+    ):
         return state
